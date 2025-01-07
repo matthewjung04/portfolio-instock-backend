fd81979ef2c23ea85416d281a01ba14dc5a40798
matthewjung04 <matthew.jung95@gmail.com>
2025-01-06 22:15:13 -0500

    update deployment knex

--- knexfile.js
+++ knexfile.js
@@ -9,7 +9,7 @@ const filename = fileURLToPath(import.meta.url);
 const dirname = path.dirname(filename);
 
 const dbUrl = process.env.JAWS_URL;
-const dbConfig = dbUrl ? parse(dbUrl) : null;
+const dbConfig = parse(dbUrl);
 
 
 export default {
@@ -31,18 +31,18 @@ export default {
   },
   production: {
     client: 'mysql2',
-    connection: dbConfig
-      ? {
-          host: dbConfig.hostname,
-          port: dbConfig.port || 3306,
-          user: dbConfig.auth.split(':')[0],
-          password: dbConfig.auth.split(':')[1],
-          database: dbConfig.pathname.slice(1),
-        }
-      : null,
+    connection: {
+        host: dbConfig.hostname,
+        port: dbConfig.port || 3306,
+        user: dbConfig.auth.split(':')[0],
+        password: dbConfig.auth.split(':')[1],
+        database: dbConfig.pathname.slice(1),
+      },
+
     migrations: {
       directory: path.join(dirname, 'migrations'),
     },
+
     seeds: {
       directory: path.join(dirname, 'seeds'),
     },
