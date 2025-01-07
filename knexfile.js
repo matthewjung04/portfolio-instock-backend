04251baab64cea4eb714c12d06b613bd9e6c123b
matthewjung04 <matthew.jung95@gmail.com>
2025-01-06 19:29:41 -0500

    fix .env file

--- knexfile.js
+++ knexfile.js
@@ -11,10 +11,10 @@ export default {
   development: {
     client: 'mysql2',
     connection: {
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
+      host: process.env.DB_HOST || '127.0.0.1',
+      user: process.env.DB_USER || 'root',
+      password: process.env.DB_PASSWORD  || 'rootroot',
+      database: process.env.DB_NAME || 'instock',
       charset: "utf8",
     },
     migrations: {
