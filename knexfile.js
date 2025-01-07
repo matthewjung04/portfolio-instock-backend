d5ba1adf46ea909e7f641600382100dbd0b1fcaa
matthewjung04 <matthew.jung95@gmail.com>
2025-01-06 22:00:58 -0500

    add knex deployment export

--- knexfile.js
+++ knexfile.js
@@ -1,27 +1,50 @@
+import { parse } from 'url';
 import dotenv from 'dotenv';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
 dotenv.config();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+
+const dbUrl = process.env.JAWS_URL;
+const dbConfig = dbUrl ? parse(dbUrl) : null;
+
 
 export default {
   development: {
     client: 'mysql2',
     connection: {
       host: process.env.DB_HOST || '127.0.0.1',
-      user: process.env.DB_USER || 'root',
-      password: process.env.DB_PASSWORD  || 'rootroot',
+      user: process.env.DB_USER || 'root', 
+      password: process.env.DB_PASSWORD || 'rootroot',
       database: process.env.DB_NAME || 'instock',
       charset: "utf8",
     },
     migrations: {
-      directory: path.join(__dirname, 'migrations'),
+      directory: path.join(dirname, 'migrations'),
     },
     seeds: {
-      directory: path.join(__dirname, 'seeds'),
+      directory: path.join(dirname, 'seeds'),
     },
-  }
+  },
+  production: {
+    client: 'mysql2',
+    connection: dbConfig
+      ? {
+          host: dbConfig.hostname,
+          port: dbConfig.port || 3306,
+          user: dbConfig.auth.split(':')[0],
+          password: dbConfig.auth.split(':')[1],
+          database: dbConfig.pathname.slice(1),
+        }
+      : null,
+    migrations: {
+      directory: path.join(dirname, 'migrations'),
+    },
+    seeds: {
+      directory: path.join(dirname, 'seeds'),
+    },
+  },
 };
